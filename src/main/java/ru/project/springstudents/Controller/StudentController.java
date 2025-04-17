ee5f71184b80a96f1cf2c7086b53dc5044e74179
raiseoff <108944101+raiseoff@users.noreply.github.com>
2025-04-18 00:50:36 +0300

    Moved business logic to service from controller.

--- src/main/java/ru/project/springstudents/Controller/StudentController.java
+++ src/main/java/ru/project/springstudents/Controller/StudentController.java
@@ -1,23 +1,26 @@
 package ru.project.springstudents.Controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.project.springstudents.Model.Student;
+import ru.project.springstudents.service.StudentService;
 
 import java.util.List;
 
 @RestController //Rest
 @RequestMapping("/api/v1/students") //адрес
-
+@AllArgsConstructor //Lombok за нас делает экземпляр service
+// (Spring добавляет Service и Controller в контекст. Они оба содержат аннотацию компонент. И раз service - StudentService, то Spring внедрит экземпляр в контроллер через конструктор.
 public class StudentController {
+
+
+    private final StudentService service;
 
     @GetMapping("")
     public List<Student> findAllStudent(){
-        return List.of(
-                Student.builder().firstName("Олег").email("oleg1@gmail.com").age(52).build(),
-                Student.builder().firstName("Олег2").email("oleg2@gmail.com").age(52).build(),
-                Student.builder().firstName("Олег3").email("oleg3@gmail.com").age(52).build()
-        );
+        return service.findAllStudent();
     }
 }
